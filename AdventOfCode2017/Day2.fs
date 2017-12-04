369b7019ef9aba4a5d6c09dad40b23a069b9caa6
Aron Adler <aronadler@hotmail.com>
2017-12-04 09:02:00 +0000

    Add day 2

---
+++ AdventOfCode2017/Day2.fs
@@ -0,0 +1,22 @@
+﻿// http://adventofcode.com/2017/day/2
+
+module Day2
+
+open System.IO
+
+let rowDiff (row : int list) =
+    (List.max row) - (List.min row) |> abs
+
+let processEachRow row = rowDiff <| List.map int row
+
+
+let rows = 
+    File.ReadAllLines "./day2.tsv" 
+    |> Array.toList
+    |> List.map (fun row -> row.Split [|'\t'|])
+    |> List.map Array.toList
+
+let main = 
+    rows
+    |> List.map processEachRow
+    |> List.sum
--- AdventOfCode2017/Program.fs
+++ AdventOfCode2017/Program.fs
@@ -2,7 +2,8 @@
 
 [<EntryPoint>]
 let main argv = 
-    Day1.main |> printf "%i"
+    Day1.main |> printf "%i\n"
+    Day2.main |> printf "%i\n"
 
     System.Console.ReadLine() |> ignore
     0
