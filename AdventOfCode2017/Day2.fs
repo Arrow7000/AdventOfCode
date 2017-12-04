9eb1f1bdbe493a0e16f0bb1547af1522a3b08dfa
Aron Adler <aronadler@hotmail.com>
2017-12-04 13:15:48 +0000

    Day 2 part 2

--- AdventOfCode2017/Day2.fs
+++ AdventOfCode2017/Day2.fs
@@ -3,20 +3,48 @@
 module Day2
 
 open System.IO
+open System.Security.Cryptography.X509Certificates
 
 let rowDiff (row : int list) =
     (List.max row) - (List.min row) |> abs
 
-let processEachRow row = rowDiff <| List.map int row
-
+let rowToInts (row : string) =
+    row.Split [|'\t'|]
+    |> Array.toList
+    |> List.map int
 
 let rows = 
     File.ReadAllLines "./day2.tsv" 
     |> Array.toList
-    |> List.map (fun row -> row.Split [|'\t'|])
-    |> List.map Array.toList
+    |> List.map rowToInts
 
 let main = 
     rows
-    |> List.map processEachRow
+    |> List.map rowDiff
+    |> List.sum
+
+
+let divideEachOther a b =
+    if a = b then None else
+
+    let fA = float a
+    let fB = float b
+    let right = fA / fB
+    let left = fB / fA
+    let rightInt = floor right
+    let leftInt = floor left
+    if rightInt = right then
+        Some <| int rightInt
+    else if leftInt = left then
+        Some <| int leftInt
+    else None
+
+let divideDivisibles row =
+    List.allPairs row row
+    |> List.choose (fun (a, b) -> divideEachOther a b)
+    |> List.item 0
+
+let part2 = 
+    rows
+    |> List.map divideDivisibles
     |> List.sum
--- AdventOfCode2017/Program.fs
+++ AdventOfCode2017/Program.fs
@@ -2,8 +2,9 @@
 
 [<EntryPoint>]
 let main argv = 
-    Day1.main |> printf "%i\n"
-    Day2.main |> printf "%i\n"
+    Day1.main |> printf "Day 1: %i\n"
+    Day2.main |> printf "Day 2: %i\n"
+    Day2.part2 |> printf "Day 2 part 2: %i\n"
 
     System.Console.ReadLine() |> ignore
     0
