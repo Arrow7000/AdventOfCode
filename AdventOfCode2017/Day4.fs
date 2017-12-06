d336a669a7f7630a7b6190867599b64875823433
Aron Adler <aronadler@hotmail.com>
2017-12-06 21:50:40 +0000

    Add Day 4

---
+++ AdventOfCode2017/Day4.fs
@@ -0,0 +1,36 @@
+﻿module Day4
+
+open System.IO
+
+type Word = string
+type Line = Word list
+
+let lines : Line list = 
+    File.ReadAllLines "./day4.txt" 
+    |> Array.toList
+    |> List.map ((fun str -> str.Split [|' '|]) >> Array.toList)
+
+let uniqueWords (words : Line) = 
+    new Set<string>(words) |> Set.toList 
+    
+let hasUniques (words : Line) = 
+    words.Length = List.length (uniqueWords words)
+
+
+let main = 
+    lines
+    |> List.filter hasUniques
+    |> List.length
+
+
+let getWordCharSet (word : Word) = new Set<char>(word)
+let anagramUniqueWords (words : Word list) = List.distinctBy getWordCharSet words
+let hasAnaUniqueWords (words: Word list) = List.length words = List.length (anagramUniqueWords words)
+
+let hasAnagrams (lines : Line list) : Line list =
+    lines |> List.filter hasAnaUniqueWords
+
+let part2 = 
+    lines
+    |> hasAnagrams
+    |> List.length
--- AdventOfCode2017/Program.fs
+++ AdventOfCode2017/Program.fs
@@ -6,6 +6,8 @@ let main argv =
     Day2.main |> printf "Day 2: %i\n"
     Day2.part2 |> printf "Day 2 part 2: %i\n"
     Day3.main |> printf "Day 3: %i\n"
+    Day4.main |> printf "Day 4: %i\n"
+    Day4.part2 |> printf "Day 4 part 2: %i\n"
 
     System.Console.ReadLine() |> ignore
     0
