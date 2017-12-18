de52bfbd1227b5d29f16c84ed943fa54f296c5d7
Aron Adler <aronadler@hotmail.com>
2017-12-18 23:07:27 +0000

    Day 7 tests

--- AdventOfCode2017.Tests/Day7Test.fs
+++ AdventOfCode2017.Tests/Day7Test.fs
@@ -4,22 +4,57 @@ open Swensen.Unquote
 open Xunit
 open Day7
 
+//let inputLines =
+//    ["wdysq (135) -> vjwuuft, oislgqy";
+//    "vjwuuft (33) -> lphki";
+//    "oislgqy (77)";
+//    "lphki (233)"]
+
 let inputLines =
-    ["wdysq (135) -> vjwuuft, oislgqy";
-    "vjwuuft (33) -> lphki";
-    "oislgqy (77)";
-    "lphki (233)"]
+    [
+        "shds (102)";
+        "sdfsdf (90)"
+        "wdysq (135) -> sdfsdf, entry0name";
+        "djfhkdf (12) -> shds, wdysq";
+    ]
+
+
+[<Fact>]
+let ``makeEntry works``() =
+
+    let entry = makeEntry "name2323" 234 ["child 1"; "child num 2"]
+    let expectedEntry = 
+        {
+            name = "name2323"
+            weight = 234
+            children = ["child 1"; "child num 2"]
+        }
+    test <@ entry = expectedEntry @>
+
+
+[<Fact>]
+let ``parseLineToEntry works``() =
+    let entryNoChild = parseLineToEntry inputLines.[0]
+    let expectedNoChild = Some <| makeEntry "shds" 102 []
+
+    let entryWithChildren = parseLineToEntry inputLines.[2]
+    let expectedNoChildren = Some <| makeEntry "wdysq" 135 ["sdfsdf"; "entry0name"]
+
+    test <@ entryNoChild = expectedNoChild @>
+    test <@ entryWithChildren = expectedNoChildren @>
 
 
 [<Fact>]
 let ``linesToEntries works``() =
+
     let entries = linesToEntries inputLines
 
     let entry2 = makeEntry "shds" 102 []
     let entry4 = makeEntry "sdfsdf" 90 []
-    let entry3 = makeEntry "shds" 102 [entry4]
-    let entry1 = makeEntry "djfhkdf" 12 [entry2; entry3]
-    let expectedEntries = [entry1; entry2; entry3; entry4]
+    let entry3 = makeEntry "wdysq" 135 [entry4.name; "entry0name"]
+    let entry1 = makeEntry "djfhkdf" 12 [entry2.name; entry3.name]
+    let expectedEntries = [entry2; entry4; entry3; entry1]
+
 
     test <@ entries = expectedEntries @>
 
--- AdventOfCode2017/Day7.fs
+++ AdventOfCode2017/Day7.fs
@@ -2,32 +2,88 @@
 
 module Day7
 open System.IO
+open System
+open System.Text.RegularExpressions
+
+type Name = string
 
 type Entry = {
     name: string
     weight: int
-    children: Map<string, Entry option>
+    children: Name list
     }
 
-type EntryOrName =
-    | Entry of Entry
-    | Name of string
+//type TreeEntry = {
+//    name: string
+//    weight: int
+//    children: Map<string, Entry option>
+//    }
 
-let makeEntry name weight (children : EntryOrName list) =
-    let entryMap = 
-        (new Map<string, Entry option>(
-            List.map (fun entry -> entry) children
-            ))
+//type EntryOrName =
+//    | Entry of Entry
+//    | Name of string
+
+
+//let childToMapItem child : string * Entry option =
+//    match child with
+//    | Entry entry -> (entry.name, Some entry)
+//    | Name entry -> (entry, None)
+
+//let makeEntry name weight (children : EntryOrName list) =
+//    let mapEntries = List.map childToMapItem children
+
+//    let entryMap = 
+//        (new Map<string, Entry option>(mapEntries))
+//    {
+//        name = name
+//        weight = weight
+//        children = entryMap
+//    }
+
+
+let makeEntry name weight children : Entry =
     {
         name = name
         weight = weight
-        children = entryMap
+        children = children
     }
 
 let getLines file =
     File.ReadAllLines file
     |> Array.toList
 
+    
+let (|LineEntryMatch|_|) line =
+    let regMatch = Regex.Match (line, "(\w+) \((\d+)\)(?: -> ([\w, ]+))?")
+    if not regMatch.Success then
+        None
+    else
+        let getGroup (i : int) = regMatch.Groups.[i]
+
+        let name, weight, childrenStr = getGroup 1, getGroup 2, getGroup 3
+
+        let children = 
+            if childrenStr.Success then
+                childrenStr.Value.Split ([|", "|], StringSplitOptions.None)
+                |> Array.toList
+                |> Some
+            else
+                None
+        
+        Some <|
+        makeEntry
+            name.Value
+            (int weight.Value)
+            (match children with
+             | Some children -> children
+             | None -> [])
+
+let parseLineToEntry line =
+    match line with
+    | LineEntryMatch entry -> Some entry
+    | _ -> None
+
 let linesToEntries lines =
     lines
-    |> List.map 
+    |> List.map parseLineToEntry
+    |> List.choose id
