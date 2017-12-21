3f29243fecaaa8d3927a7631d7310d66bd248099
Aron Adler <aronadler@hotmail.com>
2017-12-21 20:36:09 +0000

    Day 8 progress and Commons

---
+++ AdventOfCode2017/Commons.fs
@@ -0,0 +1,15 @@
+﻿module Commons
+
+open System.IO
+
+
+let getLines file =
+    File.ReadAllLines file
+    |> Array.toList
+
+let getText file =
+    File.ReadAllText file
+
+let strSplit sep str =
+    str.Split([| sep |]))
+    |> Array.toList
