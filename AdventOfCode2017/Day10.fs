4d3369bc7e0220baac5cc3749553d64b33c63e78
Aron Adler <aronadler@hotmail.com>
2017-12-30 18:31:36 +0000

    Start of day 10

--- AdventOfCode2017/Day10.fs
+++ AdventOfCode2017/Day10.fs
@@ -2,3 +2,32 @@
 
 module Day10
 
+type inputNum = int
+
+
+let hasher input int list =
+    let rec iterator (currPos : int) (hashList : int list) (remainingInput : int list) =
+        match remainingInput with
+        | [] -> ()
+        | hash :: rest ->
+            let listLen = List.length hashList
+            let indexes = 
+                [0..hash]
+                |> List.map (fun i -> (i + currPos) % listLen)
+            let toRev =
+                indexes
+                |> List.map (fun i -> hashList.[i])
+            let reved = List.rev toRev
+            let replaced =
+                hashList
+                |> List.mapi (fun i hashChar -> if List.contains i)
+
+            //let toRev = 
+            //    remainingInput
+            //    |> List.skip (currPos - 1)
+            //    |> List.take hash
+            //let reved = List.rev toRev
+            //iterator (currPos+1)    rest
+
+
+    iterator 0 [0..255] remainingInput
