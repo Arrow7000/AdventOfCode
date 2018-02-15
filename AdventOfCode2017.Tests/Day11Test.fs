970c65f6cd1811c6ed9210fbd8558e2f8154d8a4
Aron Adler <aronadler@hotmail.com>
2018-02-15 01:13:46 +0000

    Day 11 WIP

---
+++ AdventOfCode2017.Tests/Day11Test.fs
@@ -0,0 +1,124 @@
+﻿namespace Day11Test
+
+open Swensen.Unquote
+open Xunit
+open Day11
+open System.IO
+
+module ``EvenCol single movements`` =
+        
+    let from = cell 0 0
+
+    [<Fact>]
+    let ``N works`` () =
+        let result = moveCell N from
+
+        test <@ result = EvenCol (0, -1) @>
+
+    [<Fact>]
+    let ``NE works`` () =
+        let result = moveCell NE from
+
+        test <@ result = OddCol (1, -1) @>
+
+    [<Fact>]
+    let ``SE works`` () =
+        let result = moveCell SE from
+
+        test <@ result = OddCol (1, 0) @>
+
+    [<Fact>]
+    let ``S works`` () =
+        let result = moveCell S from
+
+        test <@ result = EvenCol (0, 1) @>
+
+    [<Fact>]
+    let ``SW works`` () =
+        let result = moveCell SW from
+
+        test <@ result = OddCol (-1, 0) @>
+
+    [<Fact>]
+    let ``NW works`` () =
+        let result = moveCell NW from
+
+        test <@ result = OddCol (-1, -1) @>
+
+
+
+module ``OddCol single movements`` =
+        
+    let from = cell 1 0
+
+    [<Fact>]
+    let ``N works`` () =
+        let result = moveCell N from
+
+        test <@ result = OddCol (1, -1) @>
+
+    [<Fact>]
+    let ``NE works`` () =
+        let result = moveCell NE from
+
+        test <@ result = EvenCol (2, 0) @>
+
+    [<Fact>]
+    let ``SE works`` () =
+        let result = moveCell SE from
+
+        test <@ result = EvenCol (2, 1) @>
+
+    [<Fact>]
+    let ``S works`` () =
+        let result = moveCell S from
+
+        test <@ result = OddCol (1, 1) @>
+
+    [<Fact>]
+    let ``SW works`` () =
+        let result = moveCell SW from
+
+        test <@ result = EvenCol (0, 1) @>
+
+    [<Fact>]
+    let ``NW works`` () =
+        let result = moveCell NW from
+
+        test <@ result = EvenCol (0, 0) @>
+
+module ``Quickest path`` =
+
+    [<Fact>]
+    let ``Path 1`` () =
+        let from = cell 0 0
+        let dest = applyPath [NE; NE; NE] from
+        let result = getSteps from dest
+        
+        test <@ result = 3 @>
+
+    [<Fact>]
+    let ``Path 2`` () =
+        let from = cell 0 0
+        let dest = applyPath [NE; NE; SW; SW] from
+        let result = getSteps from dest
+        
+        test <@ result = 0 @>
+
+    [<Fact>]
+    let ``Path 3`` () =
+        let from = cell 0 0
+        let dest = applyPath [NE; NE; S; S] from
+        let result = getSteps from dest
+        
+        test <@ result = 2 @>
+
+    [<Fact>]
+    let ``Path 4`` () =
+        let from = cell 0 0
+        let dest = applyPath [SE; SW; SE; SW; SW] from
+        let result = getSteps from dest
+        
+        test <@ result = 3 @>
+
+    
---
+++ AdventOfCode2017/Day11.fs
@@ -0,0 +1,122 @@
+﻿// https://adventofcode.com/2017/day/11
+
+module Day11
+
+open Commons
+
+type x = int
+type y = int
+type afterY = int
+type CellCoord = 
+    | EvenCol of x * y
+    | OddCol of x * afterY // y is 'after y'
+
+let cell x y : CellCoord =
+    if x % 2 = 0 then EvenCol (x, y) else OddCol (x, y)
+
+//type CellCoord = x * y
+
+//let isValidCell = 
+//    function
+//    | EvenCol (x, y) -> true
+//    | OddCol (x, y) when y % 1.0 = 0.5 -> true
+//    | _ -> false
+
+type Direction =
+    | N
+    | NE
+    | SE
+    | S
+    | SW
+    | NW
+
+let origin : CellCoord = EvenCol (0, 0)
+
+let moveCell (dir : Direction) (cellCrd : CellCoord) : CellCoord =
+    match cellCrd with
+    //| EvenCol (x, y) ->
+    //    match dir with
+    //    | N -> EvenCol (x, y - 1)
+    //    | NE -> OddCol (x + 1, y - 1)
+    //    | SE -> OddCol (x + 1, y)
+    //    | S -> EvenCol (x, y + 1)
+    //    | SW -> OddCol (x - 1, y)
+    //    | NW -> OddCol (x - 1, y - 1)
+    //| OddCol (x, y) ->
+    //    match dir with
+    //    | N -> OddCol (x, y - 1)
+    //    | NE -> EvenCol (x + 1, y)
+    //    | SE -> EvenCol (x + 1, y + 1)
+    //    | S -> OddCol (x, y + 1)
+    //    | SW -> EvenCol (x - 1, y + 1)
+    //    | NW -> EvenCol (x - 1, y)
+    | EvenCol (x, y) ->
+        match dir with
+        | N -> cell x (y - 1)
+        | NE -> cell (x + 1) (y - 1)
+        | SE -> cell (x + 1) y
+        | S -> cell x (y + 1)
+        | SW -> cell (x - 1) y
+        | NW -> cell (x - 1) (y - 1)
+    | OddCol (x, y) ->
+        match dir with
+        | N -> cell x (y - 1)
+        | NE -> cell (x + 1) y
+        | SE -> cell (x + 1) (y + 1)
+        | S -> cell x (y + 1)
+        | SW -> cell (x - 1) (y + 1)
+        | NW -> cell (x - 1) y
+
+
+let rec applyPath (dirs : Direction list) cell : CellCoord =
+    dirs |> List.fold (fun from dir -> moveCell dir from) cell
+
+let getDiff ax ay bx by = abs (ax - bx) + abs (ay - by)
+
+let getXY = 
+    function
+    | EvenCol (x,y) -> x,y
+    | OddCol (x,y) -> x,y
+
+let getOneCloser fromCell toCell : Direction = // only intended for cells of different types. Otherwise simple Cartesian coordinate maths will do.
+    let failMsg = "getOneCloser used on cells of same type"
+    match fromCell with
+    | EvenCol (fromX, fromY) ->
+        match toCell with
+        | OddCol (toX, toY) -> // will only need 4 for the cross-parity cells
+            if fromX < toX then
+                if fromY <= toY then SE
+                else NE
+            else
+                if fromY <= toY then SW
+                else NW
+        | EvenCol _ -> failwith failMsg
+    | OddCol (fromX, fromY) ->
+        match toCell with
+        | EvenCol (toX, toY) ->
+            if fromX < toX then 
+                if fromY >= toY then SE
+                else NE
+            else
+                if fromY >= toY then SW
+                else NW
+        | OddCol _ -> failwith failMsg
+        
+
+let getSteps (fromCell : CellCoord) (toCell : CellCoord) : int =
+    match fromCell with
+    | EvenCol (fromX, fromY) ->
+        match toCell with
+        | EvenCol (toX, toY) -> 
+            getDiff fromX fromY toX toY
+        | OddCol (toX, toY) ->
+            let x, y = moveCell (getOneCloser fromCell toCell) fromCell |> getXY                
+            1 + getDiff fromX fromY x y
+    | OddCol (fromX, fromY) ->
+        match toCell with
+        | OddCol (toX, toY) ->
+            getDiff fromX fromY toX toY
+        | EvenCol (toX, toY) -> 
+            let x, y = moveCell (getOneCloser fromCell toCell) fromCell |> getXY
+            1 + getDiff fromX fromY x y
+
