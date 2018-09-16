4474ca6bb1142d440e38ba89f53ee9fe9db967bf
Aron Adler <aronadler@hotmail.com>
2018-09-16 23:06:59 +0100

    Use printfn

--- AdventOfCode2017/Program.fs
+++ AdventOfCode2017/Program.fs
@@ -2,33 +2,33 @@
 
 [<EntryPoint>]
 let main argv = 
-    Day1.main |> printf "Day 1: %i\n"
-    Day1.part2 |> printf "Day 1 part 2: %i\n"
+    Day1.main |> printfn "Day 1: %i"
+    Day1.part2 |> printfn "Day 1 part 2: %i"
 
-    Day2.main |> printf "Day 2: %i\n"
-    Day2.part2 |> printf "Day 2 part 2: %i\n"
+    Day2.main |> printfn "Day 2: %i"
+    Day2.part2 |> printfn "Day 2 part 2: %i"
 
-    Day3.main |> printf "Day 3: %i\n"
-    Day3.part2 |> printf "Day 3 part 2: %i\n"
+    Day3.main |> printfn "Day 3: %i"
+    Day3.part2 |> printfn "Day 3 part 2: %i"
 
-    Day4.main |> printf "Day 4: %i\n"
-    Day4.part2 |> printf "Day 4 part 2: %i\n"
+    Day4.main |> printfn "Day 4: %i"
+    Day4.part2 |> printfn "Day 4 part 2: %i"
 
-    Day5.main () |> printf "Day 5: %i\n"
-    Day5.part2 () |> printf "Day 5 part 2: %i\n"
+    Day5.main () |> printfn "Day 5: %i"
+    Day5.part2 () |> printfn "Day 5 part 2: %i"
 
-    Day6.part1 |> printf "Day 6: %i\n"
-    Day6.part2 |> printf "Day 6 part 2: %i\n"
+    Day6.part1 |> printfn "Day 6: %i"
+    Day6.part2 |> printfn "Day 6 part 2: %i"
 
-    Day7.main |> printf "Day 7: %s\n"
-    Day7.part2 |> printf "Day 7 part 2: %i\n"
+    Day7.main |> printfn "Day 7: %s"
+    Day7.part2 |> printfn "Day 7 part 2: %i"
 
-    Day8.main |> printf "Day 8: %i\n"
-    Day8.part2 |> printf "Day 8 part 2: %i\n"
+    Day8.main |> printfn "Day 8: %i"
+    Day8.part2 |> printfn "Day 8 part 2: %i"
     
-    Day9.main |> printf "Day 9: %i\n"
-    Day9.part2 |> printf "Day 9 part 2: %i\n"
+    Day9.main |> printfn "Day 9: %i"
+    Day9.part2 |> printfn "Day 9 part 2: %i"
     
-    Day10.main |> printf "Day 10: %A\n"
+    Day10.main |> printfn "Day 10: %A"
     printfn "All Finished!"
     0
