73b21bade3b6149ebeb3c52028732c311e4cc322
Aron Adler <aronadler@hotmail.com>
2018-09-16 17:02:19 +0100

    Add day 4 solution tests

---
+++ AdventOfCode2017.Tests/Day4Test.fs
@@ -0,0 +1,13 @@
+﻿module Day4Test
+
+open Swensen.Unquote
+open Xunit
+open Day4
+
+[<Fact>]
+let ``Solution part 1`` () =
+    test <@ main = 466 @>
+
+[<Fact>]
+let ``Solution part 2`` () =
+    test <@ part2 = 251 @>
