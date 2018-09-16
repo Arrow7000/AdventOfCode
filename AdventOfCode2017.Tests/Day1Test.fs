64ced5f61ef6d1d480faead983c7e42df36ba0fb
Aron Adler <aronadler@hotmail.com>
2018-09-16 15:29:56 +0100

    Fix day 1 solution

--- AdventOfCode2017.Tests/Day1Test.fs
+++ AdventOfCode2017.Tests/Day1Test.fs
@@ -6,3 +6,19 @@ open Day1
 
 [<Fact>]
 let ``Solution part 1`` () = test <@ main = 1029 @>
+
+[<Theory>]
+[<InlineData("1212", 6)>]
+[<InlineData("1221", 0)>]
+[<InlineData("123425", 4)>]
+[<InlineData("123123", 12)>]
+[<InlineData("12131415", 4)>]
+let ``Halfway solution`` ((input, solution) : string * int) =
+    let expected =
+        input
+        |> Seq.map (string >> int)
+        |> part2func
+    test <@ expected = solution @>
+
+[<Fact>]
+let ``Solution part 2`` () = test <@ main = 1220 @>
--- AdventOfCode2017/Program.fs
+++ AdventOfCode2017/Program.fs
@@ -3,7 +3,7 @@
 [<EntryPoint>]
 let main argv = 
     Day1.main |> printf "Day 1: %i\n"
-    //Day1.part2 |> printf "Day 1 part 2: %i\n"
+    Day1.part2 |> printf "Day 1 part 2: %i\n"
 
     Day2.main |> printf "Day 2: %i\n"
     Day2.part2 |> printf "Day 2 part 2: %i\n"
